97f545b0f7e336cd2dc4bbd253edf82fb0a04246
nouhaila elmafdoul <nouhailaelmafdoul@gmail.com>
2024-02-21 11:56:34 +0100

    ajouter les notes

--- etudiant.java
+++ etudiant.java
@@ -1,22 +1,27 @@
+
 public class Etudiant {
     private String nom;
     private String prenom;
     private int age;
     private String matricule;
+    private double[] notes; // Tableau de notes
 
-    public Etudiant(String nom, String prenom, int age, String matricule) {
+    public Etudiant(String nom, String prenom, int age, String matricule, double[] notes) {
         this.nom = nom;
         this.prenom = prenom;
         this.age = age;
         this.matricule = matricule;
+        this.notes = notes;
     }
 
     public void afficher() {
         System.out.println("Nom: " + nom + ", Prénom: " + prenom + ", Âge: " + age + ", Matricule: " + matricule);
+        System.out.println("Notes: " + Arrays.toString(notes));
     }
 
     public static void main(String[] args) {
-        Etudiant etudiant1 = new Etudiant("Dupont", "Jean", 20, "123456");
+        double[] notesEtudiant1 = { 15.5, 12.0, 17.5 };
+        Etudiant etudiant1 = new Etudiant("Dupont", "Jean", 20, "123456", notesEtudiant1);
         etudiant1.afficher();
     }
 }
